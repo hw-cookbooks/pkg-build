47538d224166efda3d4d13de6d046313f8d541d3
Aaron Baer <aaron@hw-ops.com>
2014-01-22 17:11:24 -0800

    add parenthesis

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -58,7 +58,7 @@ if(node[:pkg_build][:isolate])
     ruby_dpkg = File.join(node[:fpm_tng][:package_dir], "#{ruby_build}.deb")
 
     commands = ["dpkg -i #{ruby_dpkg}; apt-get -f -q -y install", "gem install --no-ri --no-rdoc rubygems-update"]
-    if node[:pkg_build][:ruby][:rubygems][:version] == 'latest'
+    if (node[:pkg_build][:ruby][:rubygems][:version] == 'latest')
       commands << "gem update --system"
     else
       commands << "gem update --system #{node[:pkg_build][:ruby][:rubygems][:version]}"
